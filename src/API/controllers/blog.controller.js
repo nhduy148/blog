a3020fdbd45966424bf48dedabcaddecbba51692
nhduy148 <33407104+nhduy148@users.noreply.github.com>
2019-11-21 17:22:44 +0700

    .

--- src/API/controllers/blog.controller.js
+++ src/API/controllers/blog.controller.js
@@ -46,40 +46,41 @@ module.exports = {
     let isAdmin = true;
 
     DB.open()
-    .then( db => db.collection("categories") )
-    .then( categories => {
-      categories.find( {} ).toArray( (err,data) => {
-        if( err ) res.json({ err: "Somthing went wrong!" });
+      .then(db => db.collection("categories"))
+      .then(categories => {
+        categories.find({})
+          .toArray((err, data) => {
+            if (err) res.json({ err: "Somthing went wrong!" });
 
-        else if( !data.length > 0 ) res.json({ err: "No categories yet!" });
+            else if (!data.length > 0) res.json({ err: "No categories yet!" });
 
-        else {
-          if(isAdmin) data = data.filter( e => e.slug !== "not-released");
-          res.json(data);
-        }
+            else {
+              if (isAdmin) data = data.filter(e => e.slug !== "not-released");
+              res.json(data);
+            }
 
-        DB.close()
+            DB.close()
+          })
       })
-    })
-    .catch( err => res.json({ err: "Somthing went wrong!" }) )
+      .catch(err => res.json({ err: "Somthing went wrong!" }))
   },
 
   getHashtags: (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     DB.open()
-    .then( db => db.collection("hashtags") )
-    .then( hashtags => {
-      hashtags.find({})
-      .toArray( (err, data) => {
-        if( err ) res.json({ err: "Somthing went wrong!" });
-        
-        else if( !data.length > 0 ) res.json({ err: "No categories yet!" });
+      .then(db => db.collection("hashtags"))
+      .then(hashtags => {
+        hashtags.find({})
+          .toArray((err, data) => {
+            if (err) res.json({ err: "Somthing went wrong!" });
 
-        else res.json(data);
+            else if (!data.length > 0) res.json({ err: "No categories yet!" });
+
+            else res.json(data);
+          })
       })
-    })
-    .catch( err => res.json({ err: "Somthing went wrong!" }) )
+      .catch(err => res.json({ err: "Somthing went wrong!" }))
   },
 
   getPosts: (req, res) => {
@@ -88,50 +89,50 @@ module.exports = {
     let order = req.query.order === "ASC" ? 1 : req.query.order === "DESC" ? -1 : -1;
     let limit = parseInt(req.query.limit) || 10;
     let offSet = req.query.offSet || 0;
-    
+
     // console.log( { $sort: { [orderBy]: order} } );
 
     DB.open()
-    .then( db => db.collection("posts") )
-    .then( posts => {
-      posts.aggregate([
-        ...postQuery,
-        { $match: { status: "publish" } },
-        { $unwind: "$author" },
-        {
-          $project: {
-            _id: false,
-            post_id: 1,
-            title: 1,
-            except: 1,
-            slug: 1,
-            feature_image: 1,
-            date: 1,
-            view: 1,
-            category: 1,
-            tags: 1,
-            author: "$author.nicename",
-            comment_count: { 
-              $size: {
-                $reduce: {
-                  input: "$comments.reply",
-                  initialValue: "$comments",
-                  in: { $concatArrays : ["$$value", "$$this"] }
+      .then(db => db.collection("posts"))
+      .then(posts => {
+        posts.aggregate([
+          ...postQuery,
+          { $match: { status: "publish" } },
+          { $unwind: "$author" },
+          {
+            $project: {
+              _id: false,
+              post_id: 1,
+              title: 1,
+              except: 1,
+              slug: 1,
+              feature_image: 1,
+              date: 1,
+              view: 1,
+              category: 1,
+              tags: 1,
+              author: "$author.nicename",
+              comment_count: {
+                $size: {
+                  $reduce: {
+                    input: "$comments.reply",
+                    initialValue: "$comments",
+                    in: { $concatArrays: ["$$value", "$$this"] }
+                  }
                 }
               }
             }
-          }
-        },
-        { $sort: { [orderBy]: order} },
-        { $limit: limit },
-      ])
-      .toArray( (err, data) => {
-        if( err || !data.length > 0 ) res.json({ err: "No posts yet!" });
-        else res.json(data);
+          },
+          { $sort: { [orderBy]: order } },
+          { $limit: limit },
+        ])
+          .toArray((err, data) => {
+            if (err || !data.length > 0) res.json({ err: "No posts yet!" });
+            else res.json(data);
 
-        DB.close();
+            DB.close();
+          })
       })
-    })
-    .catch( err => res.json({ err: "Somthing went wrong!" }) )
+      .catch(err => res.json({ err: "Somthing went wrong!" }))
   }
 }
